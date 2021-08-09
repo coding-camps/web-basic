cddb8f243e7ba965d313cf2fcbe27b3fd25d2a81
JacobCN <coder211@163.com>
2021-08-10 00:41:33 +0800

    add package lodash

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import os from 'os';
 import {exec} from 'child_process';
+
 import express from 'express';
 import dotenv from 'dotenv';
+import _ from 'lodash';
+
 
 const app = express();
 app.use(express.static('public'));
@@ -29,3 +32,5 @@ console.log(url);
 
 const env = dotenv.config();
 console.log(env.parsed);
+console.log(_.VERSION);
+console.log(_.chain(env.parsed).pickBy((v,k)=>_.startsWith(k, 'DB_')).value());
