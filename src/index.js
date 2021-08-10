e019552706da60e2876d90feb4fa84dfb00c22cb
JacobCN <coder211@163.com>
2021-08-11 01:35:52 +0800

    extract app.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import _ from 'lodash';
 
+import app from './common/app.js';
 import Utils from './common/utils.js';
 
 // load configurations
 dotenv.config();
-const host = process.env.sys_host;
-const port = process.env.sys_port;
+const host = process.env.sys_host || 3000;
+const port = process.env.sys_port || 'http://localhost';
 const url = host + ":" + port;
 
 // start server
-const app = express();
 app.use(express.static('public'));
-app.listen(port);
+app.listen(port, function() {
+    console.log('express web server started on:', url)
+});
 
 // open url
 Utils.openUrl(url);
-console.log('express web server started on:', url);
