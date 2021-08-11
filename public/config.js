14f934b43d2428965848cd6c4a4500e0fc1b1f59
JacobCN <coder211@163.com>
2021-08-12 02:12:29 +0800

    add frontend framework and make them works

---
+++ public/config.js
@@ -0,0 +1,41 @@
+'use strict';
+require.config({
+    baseUrl:'.',
+    paths: {
+        'jquery': 'lib/jquery-3.6.0',
+        'underscore': 'lib/underscore-umd-1.13.1',
+        'backbone': 'lib/backbone-1.4.0',
+        'handlebars': 'lib/handlebars-v4.7.7',
+        'text': 'lib/requirejs-plugin-text-2.0.16',
+        'i18n': 'lib/requirejs-plugin-i18n-2.0.6',
+        'domReady': 'lib/requirejs-plugin-domReady-2.0.1',
+        '_str': 'lib/underscore.string-3.3.4',
+        'mymodule': 'testmodule'
+    },
+    shim: {
+        'jquery': {
+            exports: 'jQuery'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'handlebars': {
+            deps: ['jquery'],
+            exports: 'Handlebars'
+        },
+        'text': {
+            deps: ['require']
+        },
+        'i18n': {
+            deps: ['require']
+        },
+        '_str': {
+            deps: ['underscore'],
+            exports: '_s'
+        }
+    }
+});
---
+++ public/main.js
@@ -0,0 +1,24 @@
+'use strict';
+require([
+    'jquery',
+    'underscore',
+    'backbone',
+    'text',
+    'domReady',
+    'handlebars',
+    'i18n',
+    '_str',
+    'mymodule'
+], function($, _, backbone, text, domReady, handlebars, i18n, _str, mymodule) {
+    console.log('loading successfully~!')
+    console.log('require = ', require.version);
+    console.log('jquery = ', $.fn.jquery);
+    console.log('underscore =', _.VERSION);
+    console.log('backbone =', backbone.VERSION);
+    console.log('handlebars = ', handlebars.VERSION);
+    console.log('text = ', text.version);
+    console.log('i18n = ', i18n.version);
+    console.log('domReady =', domReady.version);
+    console.log('_str = ', _str.VERSION);
+    mymodule.run();
+});
